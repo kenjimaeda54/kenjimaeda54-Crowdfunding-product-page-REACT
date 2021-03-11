49c46c5889fa55f3e012cae0d023125645e5e055
kenjimaeda54 <kenjimaedafamily@gmail.com>
2021-03-11 10:49:28 -0300

    conclui header

--- componente/estilos.js
+++ componente/estilos.js
@@ -1,5 +1,7 @@
 import styled from "styled-components";
+
 import imageHero from "../assests/imageHero.jpg";
+import hamburger from "../assests/hamburger.svg";
 
 export const Content = styled.header`
   width: 100%;
@@ -14,20 +16,49 @@ export const Main = styled.div`
   height: 128px;
   background: linear-gradient(180deg, #000000 0%, rgba(0, 0, 0, 0.0001) 100%);
   padding: 30px;
-
   .img {
     margin-top: 60px;
+    transition: 3s;
+    cursor: pointer;
+    
+  }
+  .img:hover{
+    opacity:39%;
   }
 
-  .navegacao {
-    display: flex;
-    width: 100%;
-    max-width: 1440px;
-    flex-direction: row;
-    justify-content: flex-end;
-    margin-top: -20px;
-    list-style: none;
+
+  @media (min-width: 740px) {
+    .navegacao {
+      display: flex;
+      width: 100%;
+      max-width: 1440px;
+      flex-direction: row;
+      justify-content: flex-end;
+      margin-top: -20px;
+      list-style: none;
+    }
+    .imgbar {
+      display: none;
+    }
   }
+  @media (max-width: 740px) {
+    .imgbar {
+      width: 100%;
+      height: 20px;
+      max-width: 1440px;
+      display: flex;
+      justify-content: flex-end;
+      margin-top: -20px;
+    }
+    #icon {
+      width: 16px;
+      height: 16px;
+    }
+    .navegacao {
+      display: none;
+    }
+  }
+
   a {
     margin: auto 20px;
     text-decoration: none;
@@ -36,11 +67,11 @@ export const Main = styled.div`
     font-weight: 500;
     font-size: 13px;
     line-height: 16px;
-    transition: 1s;
+    transition: 3s;
     color: #ffffff;
     cursor: pointer;
   }
   a:hover {
-    color: #F5F5DC;
+    color: #000000;
   }
 `;
--- componente/header.js
+++ componente/header.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import { Content, Main } from "./estilos";
 
 import logo from "../assests/logo.svg";
+import hamburger from "../assests/hamburger.svg";
 
 export default function Header() {
   return (
@@ -12,6 +13,9 @@ export default function Header() {
           <Link to="/">
             <img className="img" src={logo} alt="imagem logo" />
           </Link>
+          <div className="imgbar">
+            <img src={hamburger} alt="imagem bar" id="icon" />
+          </div>
           <ul className="navegacao">
             <li>
               <Link to="/sobre">Sobre</Link>
